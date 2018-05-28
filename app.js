50de64dc7f49a848237a0fea2057477bd7235079
Jennifer Parra <jparra.biblio@gmail.com>
2018-05-28 00:37:11 -0400

    Initial Commit - Reto funciones matemáticas

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function add(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function multiply(numero1, numero2){
+    return numero1 * numero2;
+}
+
+var resultado1 = add(9824,777);
+var resultadoFinal = multiply(resultado1,36325);
+
+console.log(resultadoFinal);
+
+/*No se si se puede simplicar, pero lo intenté de muchas formas 
+y esta es la única que me resultó! I'm going to explode*/
